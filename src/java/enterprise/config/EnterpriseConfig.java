f647890256e3588032fb0f5b69ac8652a4273bef
Кулицкий <@ASU-2.ZSK.BY>
2013-08-02 02:04:02 +1200

    asm.jar added. Continue struggle

--- src/java/enterprise/config/EnterpriseConfig.java
+++ src/java/enterprise/config/EnterpriseConfig.java
@@ -3,11 +3,9 @@ package enterprise.config;
 import org.springframework.context.annotation.Configuration;
 
 /**
- *
+ * 
  * @author the-ramones
  */
 @Configuration
 public class EnterpriseConfig {
-    
-
 }
